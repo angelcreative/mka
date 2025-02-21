b6277e25e710fa6bae66a563f69ec07918f0f9f6
angelcreative <ajsc50@gmail.com>
2025-02-21 07:17:41 +0100

    logo

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['pdfjs-dist']
-  },
-  base: '/mka/',
+    optimizeDeps: {
+      exclude: ['pdfjs-dist']
+    },
+    base: mode === 'production' ? '/mka/' : '/',
+    define: {
+      'process.env.VITE_OPENAI_API_KEY': JSON.stringify(env.VITE_OPENAI_API_KEY)
+    }
+  }
 }) 
