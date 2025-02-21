77655d71a570d46f4fc9fa96a6163c2ed73cb3b4
angelcreative <ajsc50@gmail.com>
2025-02-21 08:32:31 +0100

    fix both problems

--- vite.config.js
+++ vite.config.js
@@ -17,15 +17,9 @@ export default defineConfig(({ mode }) => {
     base: '/mka/',
     build: {
       outDir: 'dist',
-      assetsDir: 'assets',
       rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-        },
         output: {
-          entryFileNames: 'assets/[name].[hash].js',
-          chunkFileNames: 'assets/[name].[hash].js',
-          assetFileNames: 'assets/[name].[hash].[ext]'
+          manualChunks: undefined
         }
       }
     },
