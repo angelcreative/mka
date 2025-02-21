a11a11d53a4963b12fbc727b81907195936eb1db
angelcreative <ajsc50@gmail.com>
2025-02-21 23:45:09 +0100

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,18 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-    },
-    optimizeDeps: {
-      exclude: ['pdfjs-dist']
-    },
-    base: '/mka/',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      rollupOptions: {
-        input: path.resolve(__dirname, 'index.html'),
-        output: {
-          entryFileNames: 'assets/index.js',
-          chunkFileNames: 'assets/[name].[hash].js',
-          assetFileNames: 'assets/[name].[hash][extname]'
-        }
-      }
-    },
-    define: {
-      '__OPENAI_API_KEY__': '""'
+export default defineConfig({
+  plugins: [react()],
+  base: '/mka/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 }) 
